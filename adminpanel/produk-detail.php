907ed02ac10d67960940981018c909627197c9ec
Eciciii <astriaeci363@gmail.com>
2023-11-21 10:50:06 +0700

    pertama commit

---
+++ adminpanel/produk-detail.php
@@ -0,0 +1,99 @@
+<?php
+     require "session.php";
+     require "../koneksi.php";
+
+     $id = $_GET['p'];
+
+     $query = mysqli_query($con, "SELECT a.*, b.nama AS nama_kategori FROM produk a join kategori b on a.kategori_id=b.id WHERE a.id='$id'");
+     $data = mysqli_fetch_array($query);
+
+     $queryKategori = mysqli_query($con, "SELECT * FROM kategori WHERE id!='$data[kategori_id]'");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk Detail</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</head>
+<style>
+    form div{
+        margin-bottom: 10px;
+    }
+</style>
+<body>
+    <?php require "navbar.php"?>
+    
+    <div class="container mt-5">
+        <h2>Detail Produk</h2>
+
+        <div class="col-12 col-md-6 mb-5">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div>
+                    <label for="nama">Nama</label>
+                    <input type="text" id="nama" name="nama" value="<?php echo $data['nama']?>"class="form-control" autocomplete="off" required>
+                </div>
+                <div>
+                    <label for="kategori">Kategori</label>
+                    <select name="kategori" id="kategori" class="form-control" required>
+                        <option value="<?php echo $data['kategori_id']; ?>"><?php echo $data['nama_kategori']; ?></option>
+                        <?php
+                            while($dataKategori=mysqli_fetch_array($queryKategori)){
+                        ?>
+                                <option value="<?php echo $dataKategori['id']; ?>"><?php echo $dataKategori['nama']; ?></option>
+                        <?php
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div>
+                    <label for="harga">Harga</label>
+                    <input type="number" class="form-control" value="<?php echo $data['harga']?>"name="harga" required>
+                </div>
+                <div>
+                    <label for="currentFoto">Foto Produk Sekarang</label>
+                    <img src="../image/<?php echo $data['foto']; ?>" alt="" width="300px">
+                </div>
+                <div>
+                    <label for="foto">Foto</label>
+                    <input type="file" name="foto" id="foto" class="form-control">
+                </div>
+                <div>
+                    <label for="detail">Detail</label>
+                    <textarea name="detail" id="detail" cols="30" rows="10" class="form-control">
+                        <?php echo $data['detail']; ?>
+                    </textarea>
+                </div>
+                <div>
+                    <label for="ketersediaan_stok">Ketersediaan Stok</label>
+                    <select name="ketersediaan_stok" id="ketersediaan_stok" class="form-control">
+                        <option value="<?php echo $data['ketersediaan_stok']; ?>"><?php echo $data['ketersediaan_stok']; ?></option>
+                        <?php
+                            if($data['ketersediaan_stok']=='tersedia'){
+                        ?>
+                            <option value="habis">habis</option>
+                        <?php        
+                            }
+                            else{
+                        ?>
+                            <option value="tersedia">tersedia</option>
+                        <?php
+
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
